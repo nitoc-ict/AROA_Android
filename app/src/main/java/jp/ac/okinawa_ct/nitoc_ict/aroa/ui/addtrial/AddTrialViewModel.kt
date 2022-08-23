4d63f702e5e2b8d671e5de51d37676b5808a34aa
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2022-08-23 20:02:04 +0900

    AddTrialFragmentを追加
    中央にTextViewを表示するだけです。

---
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/addtrial/AddTrialViewModel.kt
@@ -0,0 +1,13 @@
+package jp.ac.okinawa_ct.nitoc_ict.aroa.ui.addtrial
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddTrialViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is addTrial Fragment"
+    }
+    val text: LiveData<String> = _text
+}
