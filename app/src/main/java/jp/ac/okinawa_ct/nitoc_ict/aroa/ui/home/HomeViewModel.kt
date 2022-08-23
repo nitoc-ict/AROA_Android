b681c565f3be03729fad848652152cfe29e487f2
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2022-08-23 19:57:34 +0900

    HomeFragmentを追加
    中央にTextViewを表示するだけです。

---
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package jp.ac.okinawa_ct.nitoc_ict.aroa.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
