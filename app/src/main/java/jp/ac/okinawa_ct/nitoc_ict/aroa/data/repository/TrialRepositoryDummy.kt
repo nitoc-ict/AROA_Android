1e9d8fc7d9127909235b501732782d1ada546b74
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2022-09-06 23:23:12 +0900

    TrialRepositoryDummy を作成しました。

---
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/data/repository/TrialRepositoryDummy.kt
@@ -0,0 +1,109 @@
+package jp.ac.okinawa_ct.nitoc_ict.aroa.data.repository
+
+import android.util.Log
+import com.google.android.gms.maps.model.LatLng
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Result
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Trial
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+/**
+ * テスト用のダミーのTrialを返す[TrialRepository]
+ */
+class TrialRepositoryDummy : TrialRepository {
+
+    private val testDataList = mutableListOf<Trial>(
+        Trial.Marathon(
+            "テストマラソントライアル_1",
+            "testUserID",
+            LatLng(26.526230, 128.030372),  // 沖縄高専
+            listOf(
+                LatLng(26.526230, 128.030372),
+                LatLng(26.526227, 128.030645),
+                LatLng(26.526424, 128.030645),
+                LatLng(26.526424, 128.030372),
+                LatLng(26.526230, 128.030372),
+            ), // 沖縄高専の敷地内を回るコース
+            "testTrialID_1",
+        ),
+        Trial.Marathon(
+            "テストマラソントライアル_2",
+            "testUserID",
+            LatLng(26.526347, 128.029209),  // 沖縄高専
+            listOf(
+                LatLng(26.526347, 128.029209),
+                LatLng(26.526230, 128.030372),
+                LatLng(26.526227, 128.030645),
+                LatLng(26.526424, 128.030645),
+                LatLng(26.526424, 128.030372),
+                LatLng(26.526347, 128.029209),
+            ), // 沖縄高専の敷地内を回るコース
+            "testTrialID_2",
+        ),
+        Trial.Marathon(
+            "テストマラソントライアル_3",
+            "testUserID",
+            LatLng(26.525823, 128.031147),  // 沖縄高専
+            listOf(
+                LatLng(26.525823, 128.031147),
+                LatLng(26.526230, 128.030372),
+                LatLng(26.526227, 128.030645),
+                LatLng(26.526424, 128.030645),
+                LatLng(26.526424, 128.030372),
+                LatLng(26.525823, 128.031147),
+            ), // 沖縄高専の敷地内を回るコース
+            "testTrialID_3",
+        ),
+
+        )
+
+    override fun getTrialById(id: String): Flow<Result<Trial?>> =
+        flow<Result<Trial>> {
+            delay(1000) // ネットワーク処理の遅延の際限の為、1000ms 待つ
+            emit(Result.Success(testDataList[0]))
+            Log.d("TrialRepositoryDummy", testDataList[0].toString())
+        }.catch {
+            // 本来はここでエラー処理をする
+            when (it) {
+                is Exception -> {
+                    emit(Result.Error(true, it))
+                }
+            }
+        }.onStart {
+            emit(Result.Loading)
+            Log.d("TrialRepositoryDummy", "Loading...")
+        }.flowOn(Dispatchers.IO)
+
+    override fun getTriedTrialByUserId(userId: String): Flow<Result<List<Trial>>> =
+        flow<Result<List<Trial>>> {
+            delay(1000) // ネットワーク処理の遅延の際限の為、1000ms 待つ
+            emit(Result.Success(testDataList))
+            Log.d("TrialRepositoryDummy", testDataList.toString())
+        }.catch {
+            // 本来はここでエラー処理をする
+            when (it) {
+                is Exception -> {
+                    emit(Result.Error(true, it))
+                }
+            }
+        }.onStart {
+            emit(Result.Loading)
+            Log.d("TrialRepositoryDummy", "Loading...")
+        }.flowOn(Dispatchers.IO)
+
+    override fun getTrialsNear(location: LatLng, radius: Double): Flow<Result<List<Trial>>> {
+        return getTriedTrialByUserId("")
+    }
+
+    override fun createTrial(trial: Trial): Flow<Result<Trial>> =
+        flow<Result<Trial>> {
+            testDataList.add(trial)
+            delay(500)
+            emit(Result.Success(trial))
+            Log.d("TrialRepositoryDummy", testDataList.toString())
+        }.onStart {
+            emit(Result.Loading)
+            Log.d("TrialRepositoryDummy", "Updating...")
+        }.flowOn(Dispatchers.IO)
+}
