51c0f4128f523d4df9fd7da4caaae22dc588dfa1
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2022-09-14 21:55:40 +0900

    TrialRemoteDatasource の一部のメソッドの返り値が違っていたので、修正しました。

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/data/remote/TrialFierstoreDatasource.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/data/remote/TrialFierstoreDatasource.kt
@@ -1,19 +1,23 @@
 package jp.ac.okinawa_ct.nitoc_ict.aroa.data.remote
 
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Result
 import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Trial
+import kotlinx.coroutines.tasks.await
 
 class TrialFierstoreDatasource: TrialRemoteDatasource {
-    override suspend fun getTrialById(id: String): Result<Trial?> {
+
+    override suspend fun getTrialById(id: String): Response<Trial?> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getTrialNear(location: LatLng, radius: Double): Result<List<Trial>> {
+    override suspend fun getTrialNear(location: LatLng, radius: Double): Response<List<Trial>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getTriedTrialByUserId(userId: String): Result<List<Trial>> {
+    override suspend fun getTriedTrialByUserId(userId: String): Response<List<Trial>> {
         TODO("Not yet implemented")
     }
 
--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/data/remote/TrialRemoteDatasource.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/data/remote/TrialRemoteDatasource.kt
@@ -14,14 +14,14 @@ interface TrialRemoteDatasource {
      *
      * @param id 取得する[Trial]のID
      */
-    suspend fun getTrialById(id: String): Result<Trial?>
+    suspend fun getTrialById(id: String): Response<Trial?>
 
     /**
      * 指定されたユーザーが参加したことがある(記録が残っている)[Trial]のリストを取得するメソッド
      *
      * @param userId 検索したいユーザのUserID
      */
-    suspend fun getTriedTrialByUserId(userId: String): Result<List<Trial>>
+    suspend fun getTriedTrialByUserId(userId: String): Response<List<Trial>>
 
     /**
      * 指定された位置の周辺の[Trial]のリストを取得するメソッド
@@ -29,7 +29,7 @@ interface TrialRemoteDatasource {
      * @param location 中心となる位置
      * @param radius 中心からの半径.単位はメートル
      */
-    suspend fun getTrialNear(location: LatLng, radius: Double): Result<List<Trial>>
+    suspend fun getTrialNear(location: LatLng, radius: Double): Response<List<Trial>>
 
     /**
      * 引数で渡された[Trial]をデータベースに登録するメソッド
