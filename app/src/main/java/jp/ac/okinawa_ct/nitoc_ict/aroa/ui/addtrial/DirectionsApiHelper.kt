4ac5ca75192c33576f99ee36e4c04b7db25b309f
scitheAX <80904538+scitheAX@users.noreply.github.com>
2022-09-17 23:30:06 +0900

    DirectionAPIの処理を行うクラスを追加しました

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/addtrial/DirectionsApiHelper.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/addtrial/DirectionsApiHelper.kt
@@ -1,15 +1,15 @@
 package jp.ac.okinawa_ct.nitoc_ict.aroa.ui.addtrial
 
-import android.util.Log
+import android.content.Context
 import androidx.annotation.Nullable
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.DirectionsApi
 import com.google.maps.GeoApiContext
 import com.google.maps.model.DirectionsResult
-import com.google.maps.model.LatLng as MapsLatLng
+import com.google.maps.model.LatLng
 import com.google.maps.model.TravelMode
 import com.google.maps.model.Unit
 import jp.ac.okinawa_ct.nitoc_ict.aroa.BuildConfig
+import jp.ac.okinawa_ct.nitoc_ict.aroa.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.util.*
@@ -24,11 +24,11 @@ class DirectionsApiHelper {
      * @return 取得成功: [com.google.maps.model.DirectionsResult] 失敗: null
      */
     @Nullable
-    suspend fun execute(origin: MapsLatLng?, destination: MapsLatLng?, waypoints: String): DirectionsResult? {
+    suspend fun execute(context: Context, origin: LatLng?, destination: LatLng?, waypoints: String): DirectionsResult? {
         return withContext(Dispatchers.IO) {
             // Mapキーの取得.
             val apiContext = GeoApiContext.Builder()
-                .apiKey(BuildConfig.MAPS_API_KEY).build()
+                .apiKey(BuildConfig.API_KEY).build()
 
             // API実行.
             kotlin.runCatching {
@@ -40,28 +40,6 @@ class DirectionsApiHelper {
                     .origin(origin)
                     .destination(destination)
                     .waypoints(waypoints)
-                    .await()
-            }.getOrNull()
-        }
-    }
-
-    @Nullable
-    suspend fun onlyOriginDestExecute(origin: MapsLatLng?, destination: MapsLatLng?): DirectionsResult? {
-        return withContext(Dispatchers.IO) {
-            // Mapキーの取得.
-            val apiContext = GeoApiContext.Builder()
-                .apiKey(BuildConfig.MAPS_API_KEY).build()
-
-            Log.i("onlyOriginDestExecute","${origin.toString()},${destination.toString()}")
-            // API実行.
-            kotlin.runCatching {
-                DirectionsApi
-                    .newRequest(apiContext)
-                    .mode(TravelMode.WALKING)
-                    .units(Unit.METRIC)
-                    .language(Locale.JAPAN.language)
-                    .origin(origin)
-                    .destination(destination)
                     .await()
             }.getOrNull()
         }
