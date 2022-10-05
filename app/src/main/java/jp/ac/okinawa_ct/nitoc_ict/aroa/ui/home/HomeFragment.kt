ba8611a01663a6af957cdd85e66166f2107f81bf
scitheAX <80904538+scitheAX@users.noreply.github.com>
2022-10-06 00:32:20 +0900

    HomeFragment.ktにmapを追加し、トライアルのマーカーを表示できるようにしました。そして、マーカーをクリックしたら、そのトライアルの詳細画面に遷移するようにしました。

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/home/HomeFragment.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/home/HomeFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import jp.ac.okinawa_ct.nitoc_ict.aroa.R
 import jp.ac.okinawa_ct.nitoc_ict.aroa.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -17,26 +25,64 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var map : GoogleMap
+
+    private lateinit var homeViewModel: HomeViewModel
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+        moveCamera()
+        setMarkerClick(googleMap)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(callback)
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        homeViewModel.foundTrials.observe(viewLifecycleOwner) {
+            for (data in it){
+                map.addMarker(MarkerOptions().position(data.position).title(data.id))
+            }
         }
-        return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun moveCamera() {
+        val cUpdate = CameraUpdateFactory.newLatLngZoom(
+            LatLng(26.526230, 128.030372), 16f
+        )
+        map.apply {
+            moveCamera(cUpdate)
+        }
     }
+
+    private fun setMarkerClick(map: GoogleMap) {
+        homeViewModel.foundTrials.observe(viewLifecycleOwner) {
+            map.setOnMarkerClickListener{marker ->
+                marker.title?.let {
+                        val action = HomeFragmentDirections.actionHomeFragmentToTrialDetailFragment(
+                            it,true
+                        )
+                    this.findNavController().navigate(action)
+                }
+                return@setOnMarkerClickListener true
+            }
+        }
+    }
+
 }
--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/home/HomeViewModel.kt
@@ -3,11 +3,41 @@ package jp.ac.okinawa_ct.nitoc_ict.aroa.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.LatLng
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Result
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Trial
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.repository.TrialRepositoryDummy
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
+    private val trialRepository = TrialRepositoryDummy()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _foundTrials: MutableLiveData<List<Trial>> = MutableLiveData(listOf())
+    val foundTrials: LiveData<List<Trial>> = _foundTrials
+
+    init {
+        findTrialsNearMe()
     }
-    val text: LiveData<String> = _text
+
+    fun findTrialsNearMe() {
+
+        viewModelScope.launch {
+
+            trialRepository
+                .getTrialsNear(LatLng(26.526230, 128.030372), 100.0)
+                .collect {
+                    when (it) {
+                        is Result.Loading -> _foundTrials.value = listOf()
+                        is Result.Success -> {
+                            _foundTrials.value = it.data!!
+                        }
+
+                        is Result.Error -> {
+                            _foundTrials.value = listOf()
+                        }
+                    }
+                }
+        }
+    }
 }
