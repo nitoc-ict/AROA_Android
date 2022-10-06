4d0fbbbf5f8f736c063770a73b9b41d4910bc4df
scitheAX <80904538+scitheAX@users.noreply.github.com>
2022-10-06 21:12:33 +0900

    レコードの詳細画面をタイム、距離、スピード、コースが表示できるようにしました。

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/checkrecord/record_detail/RecordDetailFragment.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/checkrecord/record_detail/RecordDetailFragment.kt
@@ -5,14 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+import com.google.maps.model.DirectionsResult
 import jp.ac.okinawa_ct.nitoc_ict.aroa.R
+import jp.ac.okinawa_ct.nitoc_ict.aroa.databinding.FragmentRecordDetailBinding
 
 class RecordDetailFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
+    companion object {
+        private const val ZOOM_SIZE = 16f
+        private const val POLYLINE_WIDTH = 12f
+    }
 
+    private var _binding: FragmentRecordDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: RecordDetailViewModel
+
+    private var map: GoogleMap? = null
+    private var polyline: Polyline? = null
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
     }
 
     override fun onCreateView(
@@ -20,12 +43,82 @@ class RecordDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_record_detail, container, false)
+        _binding = FragmentRecordDetailBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(RecordDetailViewModel::class.java)
+        val args = RecordDetailFragmentArgs.fromBundle(requireArguments())
+        viewModel.setTrialId(args.trialId)
+        viewModel.setRecordId(args.recordId)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        viewModel.recordId.observe(viewLifecycleOwner, Observer {
+            viewModel.getRecordById()
+        })
+
+        viewModel.record.observe(viewLifecycleOwner, Observer {
+            viewModel.assignmentRecordDate()
+            binding.recordTime.text = viewModel.recordTime.value.toString() + "秒"
+
+        })
+
+        viewModel.trialId.observe(viewLifecycleOwner, Observer {
+            viewModel.getTrialById()
+        })
+
+        viewModel.trial.observe(viewLifecycleOwner, Observer {
+            viewModel.assignmentTrialData()
+            map?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(viewModel.trialCourse.value!!.get(0), ZOOM_SIZE)
+            )
+        })
+
+        viewModel.trialCourse.observe(viewLifecycleOwner, Observer {
+            viewModel.courseTranslate()
+            viewModel.directionApiExecute()
+            map?.addMarker(MarkerOptions().position(viewModel.origin.value!!).title("origin"))
+            map?.addMarker(MarkerOptions().position(viewModel.dest.value!!).title("dest"))
+        })
+
+        viewModel.directionsResult.observe(viewLifecycleOwner, Observer{
+            updatePolyline(it, map)
+            viewModel.getDistance()
+            binding.recordDistance.text = viewModel.trialDistance.value.toString() + "m"
+            val speed = viewModel.trialDistance.value!! / viewModel.recordTime.value!! * 3.6
+            binding.recordSpeed.text = speed.toString() + "km/h"
+        })
+    }
+
+    //Polylineを更新
+    private fun updatePolyline(directionsResult: DirectionsResult?, googleMap: GoogleMap?) {
+        googleMap ?: return
+        directionsResult ?: return
+        removePolyline()
+        addPolyline(directionsResult, googleMap)
+    }
+
+    // 線を消す.
+    private fun removePolyline() {
+        if (map != null && polyline != null) {
+            polyline?.remove()
+        }
+    }
+
+    // 線を引く
+    private fun addPolyline(directionsResult: DirectionsResult, map: GoogleMap) {
+        val polylineOptions = PolylineOptions()
+        polylineOptions.width(POLYLINE_WIDTH)
+        // ARGB32bit形式.
+        polylineOptions.color(R.color.map_polyline_stroke)
+        val decodedPath =
+            PolyUtil.decode(directionsResult.routes[0].overviewPolyline.encodedPath)
+        polyline = map.addPolyline(polylineOptions.addAll(decodedPath))
     }
 }
---
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/aroa/ui/checkrecord/record_detail/RecordDetailViewModel.kt
@@ -0,0 +1,161 @@
+package jp.ac.okinawa_ct.nitoc_ict.aroa.ui.checkrecord.record_detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.model.DirectionsResult
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Record
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Result
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.dto.Trial
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.repository.RecordRepositoryDummy
+import jp.ac.okinawa_ct.nitoc_ict.aroa.data.repository.TrialRepositoryDummy
+import jp.ac.okinawa_ct.nitoc_ict.aroa.ui.addtrial.DirectionsApiHelper
+import kotlinx.coroutines.launch
+
+class RecordDetailViewModel : ViewModel() {
+    private val trialRepositoryDummy = TrialRepositoryDummy()
+    private val recordRepositoryDummy = RecordRepositoryDummy()
+
+    private val _directionsResult = MutableLiveData<DirectionsResult?>()
+    val directionsResult: LiveData<DirectionsResult?> = _directionsResult
+
+    private val _origin = MutableLiveData<LatLng>()
+    val origin: LiveData<LatLng> get() = _origin
+
+    private val _dest = MutableLiveData<LatLng>()
+    val dest: LiveData<LatLng> get() = _dest
+
+    private val _waypoints = MutableLiveData<ArrayList<LatLng>>().apply {
+        value = ArrayList()
+    }
+    val waypoints: LiveData<ArrayList<LatLng>> get() = _waypoints
+
+    private val _recordId = MutableLiveData<String>()
+    val recordId: LiveData<String> get() = _recordId
+
+    private val _record = MutableLiveData<Record>()
+    val record: LiveData<Record> get() = _record
+
+    private val _recordTime = MutableLiveData<Long>()
+    val recordTime: LiveData<Long> get() = _recordTime
+
+    private val _trial = MutableLiveData<Trial>()
+    val trial: LiveData<Trial> get() = _trial
+
+    private val _trialId = MutableLiveData<String>()
+    val trialId: LiveData<String> get() = _trialId
+
+    private val _trialCourse = MutableLiveData<List<LatLng>>()
+    val trialCourse: LiveData<List<LatLng>> get() = _trialCourse
+
+    private val _trialDistance = MutableLiveData<Long>().apply {
+        value = 0
+    }
+    val trialDistance: LiveData<Long> get() = _trialDistance
+
+    fun setTrialId(id: String) {
+        _trialId.value = id
+    }
+
+    fun setRecordId(id: String) {
+        _recordId.value = id
+    }
+
+    fun getTrialById() {
+        viewModelScope.launch {
+            trialRepositoryDummy.getTrialById(_trialId.value!!).collect{
+                when(it) {
+                    is Result.Loading -> "Loading"
+                    is Result.Success -> {
+                        _trial.value = it.data!!
+                    }
+                    is Result.Error -> "Error"
+                }
+            }
+        }
+    }
+
+    fun getRecordById() {
+        viewModelScope.launch {
+            recordRepositoryDummy.getRecordByRecordId(_recordId.value!!).collect{
+                when(it) {
+                    is Result.Loading -> "Loading"
+                    is Result.Success -> {
+                        _record.value = it.data!!
+                    }
+                    is Result.Error -> "Error"
+                }
+            }
+        }
+    }
+
+    fun assignmentTrialData() {
+        _trial.value?.let {
+            when(it) {
+                is Trial.Marathon -> {
+                    _trialCourse.value = it.course
+                }
+                else -> {}
+            }
+        }
+    }
+
+    fun assignmentRecordDate() {
+        _record.value?.let {
+            when(it) {
+                is Record.MarathonRecord -> {
+                    _recordTime.value = it.time
+                }
+                else -> {}
+            }
+        }
+    }
+
+    fun getDistance() {
+        for (route in _directionsResult.value!!.routes) {
+            for (leg in route.legs) {
+                _trialDistance.value = _trialDistance.value?.plus(leg.distance.inMeters)
+            }
+        }
+    }
+
+    fun courseTranslate() {
+        _trialCourse.value?.let { courselatLng ->
+            _origin.value = courselatLng.first()
+            _dest.value = courselatLng.last()
+            for (latLng in courselatLng) {
+                when(latLng) {
+                    courselatLng.first() -> continue
+                    courselatLng.last() -> continue
+                    else -> _waypoints.value?.add(latLng)
+                }
+            }
+        }
+    }
+
+    private fun latLngToString(): String {
+        val sb = StringBuilder()
+        if (_waypoints != null) {
+            for (waypoint in _waypoints.value!!) {
+                sb.append(waypoint.latitude.toString() + "," + waypoint.longitude.toString() + "|")
+            }
+        }
+        return sb.toString()
+    }
+
+    fun directionApiExecute() {
+        viewModelScope.launch {
+            val waypointsString = latLngToString()
+            val result = DirectionsApiHelper().execute(
+                com.google.maps.model.LatLng(_origin.value!!.latitude, _origin.value!!.longitude),
+                com.google.maps.model.LatLng(_dest.value!!.latitude, _dest.value!!.longitude),
+                waypointsString
+            )
+            _directionsResult.value = result
+        }
+    }
+
+
+}
